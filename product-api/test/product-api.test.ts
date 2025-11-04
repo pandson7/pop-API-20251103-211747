3a2e1d0710a01fa537a8d34ad9a5793a64635b99
pandson7 <pandson@amazon.com>
2025-11-03 21:30:08 -0500

    Add product-api/test/product-api.test.ts

---
+++ product-api/test/product-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ProductApi from '../lib/product-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/product-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ProductApi.ProductApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
